fe6ac054c30889899b38282a91e49a35cf8d76d8
kevster01 <90598902+kevster01@users.noreply.github.com>
2022-01-30 13:44:35 -0800

    Add files via upload
    Solutions and working examples in the form of pictures are attached.

---
+++ Cst 150-Activity 10/Activity 10/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Corrected by Kevin Reynoso
+//Kevin Reynoso//CST 150//1/29/22//This is my own work
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here I found a fundamental error when creating a set object. It is necessary
+            //to attach a namespace from where to pull the information from. 
+            //"make some sets"
+            //Set A = new Set();
+            //Set B = new Set();
+            CST117_IC08_console.Set A = new CST117_IC08_console.Set();
+            CST117_IC08_console.Set B = new CST117_IC08_console.Set();
+            
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
+
+
+
+
---
+++ Cst 150-Activity 10/Activity 10/Set.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Corrected by Kevin Reynoso
+//Kevin Reynoso//CST 150//1/29/22//This is my own work
+
+//Lydia's code - find the errors!
+namespace CST117_IC08_console
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+       
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                //If the first value is not equal to a specific value then the automatic value would be false
+                if (val == elements[i])
+                    return true;
+                // else
+                //  return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        // Created a new set in order to add new values and leave set A unchanged 
+        public Set union(Set rhs)
+        {
+            Set C = new Set();
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                C.addElement(rhs.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                C.addElement(this.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            return C;
+        }
+
+
+    }
+
+
+}
